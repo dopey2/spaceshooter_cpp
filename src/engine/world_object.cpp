0d4e2574145b7c25b4de957f95c66ad2744262de
unknown <dani.gabor1996@gmail.com>
2025-03-28 11:11:50 +0100

    style: fix indentation in world_object.cpp file

--- src/engine/world_object.cpp
+++ src/engine/world_object.cpp
@@ -3,32 +3,31 @@
 #include <SDL3/SDL.h>
 
 class WorldObject {
+  protected:
+    float m_x;
+    float m_y;
+    float m_rotation;
+    float m_width;
+    float m_height;
 
-    protected:
-      float m_x;
-      float m_y;
-      float m_rotation;
-      float m_width;
-      float m_height;
+  public:
+    const float &x = m_x;
+    const float &y = m_y;
+    const float &rotation = m_rotation;
+    const float &width = m_width;
+    const float &height = m_height;
 
-    public:
-      const float &x = m_x;
-      const float &y = m_y;
-      const float &rotation = m_rotation;
-      const float &width = m_width;
-      const float &height = m_height;
+    WorldObject() {
+       this->m_x = 0;
+       this->m_y = 0;
+       this->m_rotation = 0;
+       this->m_width = 0;
+       this->m_height = 0;
+    }
 
-      WorldObject() {
-         this->m_x = 0;
-         this->m_y = 0;
-         this->m_rotation = 0;
-         this->m_width = 0;
-         this->m_height = 0;
-      }
-
-      virtual void load(SDL_Renderer *renderer) {}
-      virtual void unload() {}
-      virtual void render(SDL_Renderer *renderer){};
+    virtual void load(SDL_Renderer *renderer) {}
+    virtual void unload() {}
+    virtual void render(SDL_Renderer *renderer){};
 
     void setX(float x) { this->m_x = x; }
     void setY(float x) { this->m_x = x; }
