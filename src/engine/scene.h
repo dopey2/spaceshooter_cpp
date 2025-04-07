920099b649ea1fac337a68fce1aa3ac7f524426b
unknown <dani.gabor1996@gmail.com>
2025-04-08 00:52:18 +0200

    feat(engine): Update the declaration files of scene & world_object. Update property & method access modifier

--- src/engine/scene.h
+++ src/engine/scene.h
@@ -6,24 +6,22 @@
 #include "world_object.h"
 #include "application.h"
 
+class SceneManager;
+
 class Scene {
+friend class SceneManager;
+private:
+    void callOnUpdateCallback(Uint64 delta);
+    void renderAllObjects(SDL_Renderer* m_renderer);
 protected:
     std::vector<WorldObject*> objects;
     Application* application;
-
+    // callback to be override
+    virtual void onUpdate(Uint64 delta);
 public:
     Scene();
     Scene(std::string key);
     ~Scene();
-
-    // callback to be override
-    virtual void onUpdate(Uint64 delta);
-
     void addObject(WorldObject* object);
     void removeObject(WorldObject* object);
-
-
-    // make private and friend of scene manager
-    void callOnUpdateCallback(Uint64 delta);
-    void renderAllObjects(SDL_Renderer* m_renderer);
 };
--- src/engine/world_object.h
+++ src/engine/world_object.h
@@ -4,17 +4,20 @@
 #include <optional>
 
 class Application;
+class Scene;
 
 class WorldObject {
-  protected:
+friend class Scene;
+protected:
     Application* m_application;
+    virtual void onUpdate(Uint64 delta);
 
-  public:
-  float* m_x = nullptr;
-  float* m_y = nullptr;
-  float* m_width = nullptr;
-  float* m_height = nullptr;
-  float* m_rotation = nullptr;
+public:
+    float* m_x = nullptr;
+    float* m_y = nullptr;
+    float* m_width = nullptr;
+    float* m_height = nullptr;
+    float* m_rotation = nullptr;
 
     WorldObject();
     virtual ~WorldObject();
@@ -22,7 +25,6 @@ class WorldObject {
     virtual void load(SDL_Renderer *renderer);
     virtual void unload();
     virtual void render(SDL_Renderer *renderer);
-    virtual void onUpdate(Uint64 delta);
 
     void setX(float x);
     void setY(float x);
