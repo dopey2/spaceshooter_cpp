c398a4f4460792f103db7b0d95fd51d1f93f9e19
unknown <dani.gabor1996@gmail.com>
2025-04-10 12:56:42 +0200

    feat(engine): add a prepocessor directive to check if logger functions should be implemented

--- src/engine/logger.cpp
+++ src/engine/logger.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 
+#define ENABLE_LOGGER 0
+
 namespace Logger {
 
     void debug(std::string str) {
+        #if ENABLE_LOGGER == 1
         std::cout << str << std::endl;
+        #endif
     }
 
     void error(std::string str) {
